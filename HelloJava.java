e8f2f0c2318941f3cb4d030bb2ec3da30f3cc9ed
rodrigdm <74906449+rodrigdm@users.noreply.github.com>
2021-09-03 11:50:00 +0100

    Update README.md (#1)
    * Update README.md
    
    * Update HelloJava.java
    
    * Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -2,7 +2,8 @@ ass HelloJava {
 
 public static void main(String args[]){
 
-System.out.Println(“ ------- I am your Java Evolution Program. Thank you! ”);
+System.out.Println(“ ------- I am your Java Evolution Program. Thank you! ”);  
+System.out.Println(“ ------- First Branch in the project! ”);
 
 }
 
